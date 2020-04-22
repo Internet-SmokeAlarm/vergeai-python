3e074704c71f4f95411d5e309b3f32469fce5bb9
Vale Tolpegin <valetolpegin@gmail.com>
2020-04-22 11:48:05 -0400

    Update integration tests to reflect API changes in back-end

--- tests/api/integration_tests/get_group.py
+++ tests/api/integration_tests/get_group.py
@@ -1,19 +1,13 @@
-import unittest
+from .base import BaseTest
 
-from fedlearn import FedLearnApi
 from fedlearn.exceptions import FedLearnApiException
 
-from .get_env_vars import load_env_vars
-
-class IT_GetGroupTestCase(unittest.TestCase):
+class IT_GetGroupTestCase(BaseTest):
 
     def test_pass(self):
-        cloud_gateway_url, api_key = load_env_vars()
-        client = FedLearnApi(cloud_gateway_url, api_key)
-        group = client.create_group("sim_test_group")
+        group = self.client.create_group("sim_test_group")
 
-        returned_group = client.get_group(group.get_id())
-
+        returned_group = self.client.get_group(group.get_id())
         self.assertEqual(returned_group.get_id(), group.get_id())
 
-        client.delete_group(group.get_id())
+        self.client.delete_group(group.get_id())
